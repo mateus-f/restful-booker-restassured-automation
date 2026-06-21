91484091e1623343881fb475dd4c174b5e9c0111
Mateus Ferreira <mateusjunior803@gmail.com>
2026-06-20 21:16:26 -0300

    :tada: init(test-automation): add suites and runners structure

---
+++ src/test/java/br/com/base/BaseTest.java
@@ -0,0 +1,12 @@
+package br.com.base;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class BaseTest {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+    }
+}
---
+++ src/test/java/br/com/runners/IntegrationTests.java
@@ -0,0 +1,14 @@
+package br.com.runners;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.suites.IntegrationTests.class)
+@Suite.SuiteClasses({
+
+})
+public class IntegrationTests {
+}
